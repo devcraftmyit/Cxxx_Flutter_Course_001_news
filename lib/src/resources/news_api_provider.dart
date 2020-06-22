e60d52d23c37cf620494460ad09b388e12409f18
robt <dev.craftmyit.co.uk>
2020-06-22 17:26:56 +0100

    223 Fetching Individual Items

---
+++ lib/src/resources/news_api_provider.dart
@@ -0,0 +1,20 @@
+import 'package:http/http.dart' show Client;
+import 'dart:convert';
+import '../models/item_model.dart';
+
+class NewsItemProvider {
+  Client client = Client();
+  final root = 'https://hacker-news.firebaseio.com/v0';
+
+  fetchTopIds(dynamic) async {
+    final response = await client.get('$root /topstories.json');
+    final ids = json.decode(response.body);
+    return ids;
+  }
+
+  fetchItem(int id) async {
+    final response = await client.get('$root /item/id');
+    final parsedJson = json.decode(response.body);
+    return ItemModel.fromJson(parsedJson);
+  }
+}
