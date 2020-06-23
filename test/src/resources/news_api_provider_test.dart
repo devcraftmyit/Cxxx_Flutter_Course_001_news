6d405331fd0a0ef1b377c45038868e138054dcf9
robt <dev.craftmyit.co.uk>
2020-06-23 12:14:35 +0100

    226 Writing Expectations

--- test/src/resources/news_api_provider_test.dart
+++ test/src/resources/news_api_provider_test.dart
@@ -1,5 +1,16 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:news/src/models/item_model.dart';
 import 'package:news/src/resources/news_api_provider.dart';
 import 'package:http/http.dart';
 import 'dart:convert';
 import 'package:http/http.dart';
 import 'package:http/testing.dart';
+
+void main() {
+  test('FetchTopIds returns a list of  [ ids ]', () {
+    //set up of test case //
+    final sum = 1 + 3;
+    // expectations //
+    expect(sum, 4);
+  });
+}
