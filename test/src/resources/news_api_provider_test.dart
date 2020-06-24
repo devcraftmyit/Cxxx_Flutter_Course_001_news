1ddf0d0814e4166da9c2add7d15b11798e69c1c8
robt <dev.craftmyit.co.uk>
2020-06-24 10:30:24 +0100

    228 Returning JSON

--- test/src/resources/news_api_provider_test.dart
+++ test/src/resources/news_api_provider_test.dart
@@ -1,17 +1,22 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:news/src/models/item_model.dart';
 import 'package:news/src/resources/news_api_provider.dart';
 import 'package:http/http.dart';
 import 'dart:convert';
-import 'package:http/http.dart';
 import 'package:http/testing.dart';
 
 void main() {
   test(
     'FetchTopIds returns a list of  [ ids ]',
-    () {
-      final newItemIds = NewsItemProvider();
-      MockClient(request)
+    () async {
+      final newsApi = NewsItemProvider();
+      newsApi.client = MockClient(
+        (request) async {
+          return Response(json.encode([1, 2, 3, 4]), 200);
+        },
+      );
+      // call the app with the above
+      final ids = await newsApi.fetchTopIds(dynamic);
+      expect (ids, [1, 2, 3, 4]);
     },
   );
 }
