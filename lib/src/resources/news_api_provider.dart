0679eda77d3489e9c4a88d5adbd1db43adaf6546
robt <dev.craftmyit.co.uk>
2020-06-30 15:38:24 +0100

    243 Casting Lists

--- lib/src/resources/news_api_provider.dart
+++ lib/src/resources/news_api_provider.dart
@@ -9,7 +9,7 @@ class NewsApiProvider {
   Future<List<int>> fetchTopIds(dynamic) async {
     final response = await client.get('$root /topstories.json');
     final ids = json.decode(response.body);
-    return ids;
+    return ids.cast<int>();
   }
 
   Future<ItemModel> fetchItem(int id) async {
