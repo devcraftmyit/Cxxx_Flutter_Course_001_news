80f9ba941a7d99327a5a5cede03cc6237ddef9a1
robt <dev.craftmyit.co.uk>
2020-07-01 11:19:04 +0100

    246 The need for Abstract classes

--- lib/dartPad/245 Abstract Classes.dart
+++ lib/dartPad/245 Abstract Classes.dart
@@ -1,15 +1,28 @@
+void main() {
+  PowerGrid grid = PowerGrid();
+  NuclearPowerPlant plant = NuclearPowerPlant();
+  SolarPlant solar = SolarPlant();
+  grid.addPlant(plant);
+  grid.addPlant(solar);
+}
+
 class PowerGrid {
-  
   List<NuclearPowerPlant> connectedPlants = [];
-  
-  addPlant(NuclearPowerPlant plant){
+
+  addPlant(NuclearPowerPlant plant) {
     plant.turnOn();
     connectedPlants.add(plant);
   }
 }
 
 class NuclearPowerPlant {
-  turnOn(){
-    print ('power plant is turned on')
+  turnOn() {
+    print('power plant is turned on');
+  }
+}
+
+class SolarPlant {
+  turnOn() {
+    print('Solar plant turned on');
   }
 }
