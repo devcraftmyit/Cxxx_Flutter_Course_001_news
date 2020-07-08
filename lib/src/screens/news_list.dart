3f818db938217817b763733a23b48b850fe6c2a9
robt <dev.craftmyit.co.uk>
2020-07-08 09:49:43 +0100

    258 A Touch of BoilerPlate

---
+++ lib/src/screens/news_list.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class NewsList extends StatelessWidget{
+  Widget build(context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text ('News')
+      ),
+      body: Text ('news list'),
+    );
+  }
+}
+  
