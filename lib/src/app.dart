04f9a01118ea769589131d19b93b890a6c52c078
robt <dev.craftmyit.co.uk>
2020-07-10 12:40:45 +0100

    265 Wiring up the Stories Provider

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:news/src/blocks/stories_provider.dart';
 import 'screens/news_list.dart';
+import 'blocks/stories_bloc.dart';
 
 class App extends StatelessWidget {
-  Widget build (context){
-    return MaterialApp(
-      title: 'News', 
-      home: NewsList(),
+  Widget build(context) {
+    return StoriesProvider(
+      child: MaterialApp(
+        title: 'News',
+        home: NewsList(),
+      ),
     );
   }
 }
--- lib/src/screens/news_list.dart
+++ lib/src/screens/news_list.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'dart:async';
+import '../blocks/stories_provider.dart';
 
 class NewsList extends StatelessWidget {
   Widget build(context) {
+    final bloc = StoriesProvider.of(context);
     return Scaffold(
       appBar: AppBar(
         title: Text('News'),
