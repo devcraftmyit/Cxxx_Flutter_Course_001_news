28e44b6673223877da0c17bd5143639494612065
robt <dev.craftmyit.co.uk>
2020-07-10 09:57:50 +0100

    263 BLOC Design

--- lib/src/blocks/stories_bloc.dart
+++ lib/src/blocks/stories_bloc.dart
@@ -1,1 +1,15 @@
-class StoriesBloc {}
+import 'package:flutter/cupertino.dart';
+import 'package:rxdart/rxdart.dart';
+import '../models/item_model.dart';
+import '../resources/repository.dart';
+
+class StoriesBloc {
+
+  final _topIds = PublishSubject<List<int>>();
+
+  Observable<List<int>> get topIds => _topIds.stream;
+
+  dispose(){
+    _topIds.close();
+  }
+}
